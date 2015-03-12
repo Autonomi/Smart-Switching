5de3f2d7ccf1ba4d1e0b34d2fe241bbe921afba6
Prashant Sinha <prashnts@live.com>
2015-03-12 22:22:55 +0530

    Setup

--- init.js
+++ init.js
@@ -2,13 +2,21 @@
 
 // Set up app and DB objects
 var express = require('express'),
-    app = express();
+    app = express(),
+    device = require('./model/device');
+
 
 app.get("/", function (request, response) {
     "use strict";
-    console.log(request);
     response.send("API Root.");
 });
+
+
+app.get("/_/msg", function (request, response) {
+    "use strict";
+    response.send("Recieve your outstanding messages from here.");
+});
+
 
 var server = app.listen(3000, function () {
     "use strict";
